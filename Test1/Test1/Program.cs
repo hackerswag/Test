367a3860f9b7da73853a38d9d2fd1356f6ade474
Emil Eneroth <emen02001@utb.vaxjo.se>
2020-11-02 08:44:41 +0100

    Version 1

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int c = a + b;
+            Console.WriteLine(c);
+               
+        }
+    }
+}
